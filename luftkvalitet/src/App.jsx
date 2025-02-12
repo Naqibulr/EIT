d48096aa3635f28e0a5d66424d3da5e40ab0c20a
naqibulr <naqibulr@stud.ntnu.no>
2025-02-12 08:59:11 +0100

    trafikk data

--- luftkvalitet/src/App.jsx
+++ luftkvalitet/src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"; // Importing necessary components
+import "leaflet/dist/leaflet.css";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -28,6 +30,23 @@ function App() {
       <p className="read-the-docs bg-red-400">
         Click on the Vite and React logos to learn more about them.
       </p>
+      <div id="map">
+        <MapContainer
+          center={[51.505, -0.09]}
+          zoom={13}
+          scrollWheelZoom={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
     </>
   );
 }
