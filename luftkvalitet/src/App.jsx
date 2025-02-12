ac76a104c19072524dc3c80377454f961fc3a50a
naqibulr <naqibulr@stud.ntnu.no>
2025-02-12 15:20:05 +0100

    Add map

--- luftkvalitet/src/App.jsx
+++ luftkvalitet/src/App.jsx
@@ -1,50 +1,29 @@
-import { useState } from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"; // Importing necessary components
 import "leaflet/dist/leaflet.css";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
+import { MapContainer, TileLayer, Rectangle } from "react-leaflet";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const rectangle = [
+    [63.420827, -0.28],
+    [63.120827, -0.28],
+  ];
+  const blackOptions = { color: "black" };
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs bg-red-400">
-        Click on the Vite and React logos to learn more about them.
-      </p>
-      <div id="map">
+      <div style={{ height: "97vh", width: "95vw" }}>
         <MapContainer
-          center={[51.505, -0.09]}
-          zoom={13}
-          scrollWheelZoom={false}
+          center={[63.420827, 10.391806]}
+          zoom={16}
+          scrollWheelZoom={true}
+          className="map leaflet-container"
+          style={{ height: "100%", width: "100%" }}
         >
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Marker position={[51.505, -0.09]}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
+          <Rectangle bounds={rectangle} pathOptions={blackOptions} />
         </MapContainer>
       </div>
     </>
