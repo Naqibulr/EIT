fa1faca85d1857c1ae0eae401f13e1a3d35b0193
naqibulr <naqibulr@stud.ntnu.no>
2025-03-11 17:09:05 +0100

    Enkelt modus = Done?

--- luftkvalitet/src/App.jsx
+++ luftkvalitet/src/App.jsx
@@ -9,11 +9,33 @@ import "./App.css";
 
 function App() {
   const [selected, setSelected] = useState("enkel");
+  const [summary, setSummary] = useState({
+    trafikkMendge: 0,
+    NO2: 0.0,
+    PM25: 0.0,
+    PM10: 0.0,
+  });
+  console.log(summary);
+  const thresholds = {
+    NO2: { green: 100, yellow: 200, red: 400 },
+    PM25: { green: 30, yellow: 50, red: 150 },
+    PM10: { green: 60, yellow: 120, red: 400 },
+  };
 
   const bounds = [
     [63.425, 10.404],
     [63.416, 10.388],
   ];
+
+  const isYellow =
+    summary.NO2 >= thresholds.NO2.green ||
+    summary.PM25 >= thresholds.PM25.green ||
+    summary.PM10 >= thresholds.PM10.green;
+
+  const isRed =
+    summary.NO2 >= thresholds.NO2.yellow ||
+    summary.PM25 >= thresholds.PM25.yellow ||
+    summary.PM10 >= thresholds.PM10.yellow;
 
   return (
     <>
@@ -36,30 +58,61 @@ function App() {
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <SVGOverlay bounds={bounds}>
+          <SVGOverlay key={JSON.stringify(summary)} bounds={bounds}>
             <svg>
               <defs>
                 <linearGradient id="grad1" x1="0%" y1="28%" x2="100%" y2="0%">
-                  <stop
-                    offset="0%"
-                    style={{ stopColor: "green", stopOpacity: 0.6 }}
-                  />
-                  <stop
-                    offset="30%"
-                    style={{ stopColor: "orange", stopOpacity: 0.6 }}
-                  />
-                  <stop
-                    offset="37%"
-                    style={{ stopColor: "red", stopOpacity: 0.6 }}
-                  />
-                  <stop
-                    offset="44%"
-                    style={{ stopColor: "orange", stopOpacity: 0.6 }}
-                  />
-                  <stop
-                    offset="76%"
-                    style={{ stopColor: "green", stopOpacity: 0.6 }}
-                  />
+                  {isRed ? (
+                    <>
+                      <stop
+                        offset="0%"
+                        style={{ stopColor: "green", stopOpacity: 0.6 }}
+                      />
+                      <stop
+                        offset="30%"
+                        style={{ stopColor: "orange", stopOpacity: 0.6 }}
+                      />
+                      <stop
+                        offset="37%"
+                        style={{ stopColor: "red", stopOpacity: 0.6 }}
+                      />
+                      <stop
+                        offset="44%"
+                        style={{ stopColor: "orange", stopOpacity: 0.6 }}
+                      />
+                      <stop
+                        offset="76%"
+                        style={{ stopColor: "green", stopOpacity: 0.6 }}
+                      />
+                    </>
+                  ) : isYellow ? (
+                    <>
+                      <stop
+                        offset="0%"
+                        style={{ stopColor: "green", stopOpacity: 0.6 }}
+                      />
+
+                      <stop
+                        offset="37%"
+                        style={{ stopColor: "orange", stopOpacity: 0.6 }}
+                      />
+                      <stop
+                        offset="70%"
+                        style={{ stopColor: "green", stopOpacity: 0.6 }}
+                      />
+                    </>
+                  ) : (
+                    <>
+                      <stop
+                        offset="0%"
+                        style={{ stopColor: "green", stopOpacity: 0.6 }}
+                      />
+                      <stop
+                        offset="100%"
+                        style={{ stopColor: "green", stopOpacity: 0.6 }}
+                      />
+                    </>
+                  )}
                 </linearGradient>
               </defs>
               <rect width="100%" height="100%" fill="url(#grad1)" />
@@ -126,7 +179,11 @@ function App() {
                 Avansert
               </Button>
             </div>
-            {selected === "enkel" ? <Enkel /> : <Avansert />}
+            {selected === "enkel" ? (
+              <Enkel setSummary={setSummary} />
+            ) : (
+              <Avansert />
+            )}
           </div>
         </div>
       </div>
--- luftkvalitet/src/components/Enkel.tsx
+++ luftkvalitet/src/components/Enkel.tsx
@@ -2,18 +2,39 @@ import { TextField } from "@mui/material";
 import React, { useState, useEffect } from "react";
 import DatePicker from "react-datepicker";
 import Papa from "papaparse";
+import { green, purple, red, orange } from "@mui/material/colors";
 
-const Enkel = () => {
+const Enkel = ({ setSummary }) => {
   const [startDateTime, setStartDateTime] = useState<Date | null>(
     new Date("2022-10-01T00:00:00")
   );
   const [data, setData] = useState<any[]>([]);
+  const [trafikkData, setTrafikkData] = useState<any[]>([]);
   const [filteredData, setFilteredData] = useState({
-    antallKjørtøy: 5000,
+    trafikkMendge: 0,
     NO2: 0.0,
     PM25: 0.0,
     PM10: 0.0,
   });
+
+  const thresholds = {
+    NO2: {
+      green: 100,
+      yellow: 200,
+      red: 400,
+    },
+    PM25: {
+      green: 30,
+      yellow: 50,
+      red: 150,
+    },
+    PM10: {
+      green: 60,
+      yellow: 120,
+      red: 400,
+    },
+  };
+
   useEffect(() => {
     fetch("/luftkvalitet.csv")
       .then((response) => response.text())
@@ -21,21 +42,33 @@ const Enkel = () => {
         Papa.parse(csvData, {
           complete: (result) => {
             setData(result.data);
-            console.log(result.data);
           },
           header: true,
           skipEmptyLines: true,
         });
       });
   }, []);
+  useEffect(() => {
+    fetch("/trafikkdata.csv")
+      .then((response) => response.text())
+      .then((csvData) => {
+        Papa.parse(csvData, {
+          complete: (result) => {
+            setTrafikkData(result.data);
+          },
+          header: true,
+          skipEmptyLines: true,
+          delimiter: ";",
+        });
+      });
+  }, []);
 
   useEffect(() => {
-    if (data.length === 0) return;
+    if (data.length === 0 || trafikkData.length === 0) return;
 
-    // Convert selected date to "DD.MM.YYYY HH:mm" to match Fra-tid format
     if (!startDateTime) return;
 
-    const formattedDate = startDateTime
+    const formattedAirQualityDate = startDateTime
       .toLocaleString("nb-NO", {
         year: "numeric",
         month: "2-digit",
@@ -43,37 +76,58 @@ const Enkel = () => {
         hour: "2-digit",
         minute: "2-digit",
       })
-      .replace(",", ""); // Remove comma between date and time
+      .replace(",", "");
+    const formattedTrafficDate = startDateTime.toISOString().split("T")[0];
+    const formattedTrafficTime = startDateTime.toTimeString().slice(0, 5);
+    const matchingAirQualityRow = data.find(
+      (row) => row["Fra-tid"] === formattedAirQualityDate
+    );
+    const matchingTrafficRow = trafikkData.find(
+      (row) =>
+        row["Dato"] === formattedTrafficDate &&
+        row["Fra tidspunkt"] === formattedTrafficTime
+    );
 
-    console.log("Formatted Date:", formattedDate);
-
-    // Find the row that matches startDateTime
-    const matchingRow = data.find((row) => row["Fra-tid"] === formattedDate);
-
-    if (matchingRow) {
-      setFilteredData({
-        antallKjørtøy: 5000,
-        NO2:
-          parseFloat(
-            parseFloat(matchingRow["Elgeseter NO2µg/m³"]).toFixed(2)
-          ) || 0.0,
-        PM25:
-          parseFloat(
-            parseFloat(matchingRow["Elgeseter PM2.5µg/m³"]).toFixed(2)
-          ) || 0.0,
-        PM10:
-          parseFloat(
-            parseFloat(matchingRow["Elgeseter PM10µg/m³"]).toFixed(2)
-          ) || 0.0,
-      });
-    } else {
-      setFilteredData({
-        antallKjørtøy: 5000,
-        NO2: 0.0,
-        PM25: 0.0,
-        PM10: 0.0,
-      });
-    }
+    setFilteredData({
+      NO2: matchingAirQualityRow
+        ? parseFloat(
+            parseFloat(matchingAirQualityRow["Elgeseter NO2µg/m³"]).toFixed(2)
+          ) || 0.0
+        : 0.0,
+      PM25: matchingAirQualityRow
+        ? parseFloat(
+            parseFloat(matchingAirQualityRow["Elgeseter PM2.5µg/m³"]).toFixed(2)
+          ) || 0.0
+        : 0.0,
+      PM10: matchingAirQualityRow
+        ? parseFloat(
+            parseFloat(matchingAirQualityRow["Elgeseter PM10µg/m³"]).toFixed(2)
+          ) || 0.0
+        : 0.0,
+      trafikkMendge: matchingTrafficRow
+        ? parseInt(matchingTrafficRow["Trafikkmengde"]) || 0
+        : 0,
+    });
+    setSummary({
+      NO2: matchingAirQualityRow
+        ? parseFloat(
+            parseFloat(matchingAirQualityRow["Elgeseter NO2µg/m³"]).toFixed(2)
+          ) || 0.0
+        : 0.0,
+      PM25: matchingAirQualityRow
+        ? parseFloat(
+            parseFloat(matchingAirQualityRow["Elgeseter PM2.5µg/m³"]).toFixed(2)
+          ) || 0.0
+        : 0.0,
+      PM10: matchingAirQualityRow
+        ? parseFloat(
+            parseFloat(matchingAirQualityRow["Elgeseter PM10µg/m³"]).toFixed(2)
+          ) || 0.0
+        : 0.0,
+      trafikkMendge: matchingTrafficRow
+        ? parseInt(matchingTrafficRow["Trafikkmengde"]) || 0
+        : 0,
+    });
   }, [startDateTime, data]);
 
   return (
@@ -85,6 +139,7 @@ const Enkel = () => {
           showTimeSelect
           timeIntervals={60}
           timeCaption="Hour"
+          timeFormat="HH:mm"
           dateFormat="MMMM d, yyyy HH"
           popperPlacement="right-end"
           customInput={
@@ -124,7 +179,7 @@ const Enkel = () => {
           }}
         >
           <h4>Antall Kjørtøy</h4>
-          <h4>5000</h4>
+          <h4>{filteredData.trafikkMendge}</h4>
         </div>
         <div
           style={{
@@ -133,6 +188,14 @@ const Enkel = () => {
             alignItems: "center",
             border: "1px solid black",
             padding: "0 10px",
+            backgroundColor:
+              filteredData.NO2 < thresholds.NO2.green
+                ? green[500]
+                : filteredData.NO2 < thresholds.NO2.yellow
+                ? orange[500]
+                : filteredData.NO2 < thresholds.NO2.red
+                ? red[500]
+                : purple[500],
           }}
         >
           <h4>NO2</h4>
@@ -145,6 +208,14 @@ const Enkel = () => {
             alignItems: "center",
             border: "1px solid black",
             padding: "0 10px",
+            backgroundColor:
+              filteredData.PM25 < thresholds.PM25.green
+                ? green[500]
+                : filteredData.PM25 < thresholds.PM25.yellow
+                ? orange[500]
+                : filteredData.PM25 < thresholds.PM25.red
+                ? red[500]
+                : purple[500],
           }}
         >
           <h4>PM2.5</h4>
@@ -157,6 +228,14 @@ const Enkel = () => {
             alignItems: "center",
             border: "1px solid black",
             padding: "0 10px",
+            backgroundColor:
+              filteredData.PM10 < thresholds.PM10.green
+                ? green[500]
+                : filteredData.PM10 < thresholds.PM10.yellow
+                ? orange[500]
+                : filteredData.PM10 < thresholds.PM10.red
+                ? red[500]
+                : purple[500],
           }}
         >
           <h4>PM10</h4>
